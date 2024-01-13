a01ce91ab3edfde2251ae7d9e88624ec84fb20aa
Nicolas-Moises <nicolas.nicc21@gmail.com>
2024-01-13 01:33:42 -0300

    feat: config error handlers

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,21 @@
 import fastify from 'fastify'
 import { appRoutes } from '@/http/routes'
+import { ZodError } from 'zod'
+import { env } from './env'
 
 export const app = fastify()
 
 app.register(appRoutes)
+
+app.setErrorHandler((error, _, reply) => {
+    if(error instanceof ZodError) {
+        return reply.status(400).send({ message: 'Validation error', problems: error.format() })
+    }
+
+    if(env.NODE_ENV !== 'production') {
+        console.error(error)
+    } else {
+        //TODO here we should log to an external tool like Datagog/NewRelic/Webhook to discord
+    }
+    return reply.status(500).send({ message: 'Internal server error' })
+})
