fd990b6a1df506c0ac5818291c4b520e6f1290fc
Nicolas-Moises <nicolas.nicc21@gmail.com>
2024-01-21 03:12:26 -0300

    feat: authenticate functionallity

--- src/env/index.ts
+++ src/env/index.ts
@@ -5,6 +5,7 @@ import { z } from 'zod'
 const envSchema = z.object({
     NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
     PORT: z.coerce.number().default(3333),
+    JWT_SECRET: z.string()
     // DB_HOST: z.string().default('localhost'),
     // DB_USER: z.string().default('root'),
     // DB_PASSWORD: z.string().default('<PASSWORD>'),
