33fdc58dda3796288b79e6c9bbea46d4acf0eb72
Nicolas-Moises <nicolas.nicc21@gmail.com>
2024-02-11 01:32:01 -0300

    fix: create checkin controller

--- src/http/controllers/check-ins/create.ts
+++ src/http/controllers/check-ins/create.ts
@@ -4,32 +4,29 @@ import { makeCheckInUseCase } from '@/use-cases/factories/make-check-in-use-case
 
 export async function create(request: FastifyRequest, reply: FastifyReply) {
     const createCheckInParamsSchema = z.object({
-        gymId: z.string().uuid()
+        gymId: z.string().uuid(),
     })
+
     const createCheckInBodySchema = z.object({
-        latitude: z.coerce.number().refine((value) => {
+        latitude: z.number().refine((value) => {
             return Math.abs(value) <= 90
         }),
-        longitude: z.coerce.number().refine((value) => {
+        longitude: z.number().refine((value) => {
             return Math.abs(value) <= 180
         }),
     })
 
     const { gymId } = createCheckInParamsSchema.parse(request.params)
-    const createCheckInUseCase = makeCheckInUseCase()
+    const { latitude, longitude } = createCheckInBodySchema.parse(request.body)
 
-    const { 
-        latitude, 
-        longitude
-    } = createCheckInBodySchema.parse(request.query)
+    const checkInUseCase = makeCheckInUseCase()
 
-  
-    await createCheckInUseCase.execute({
+    await checkInUseCase.execute({
+        gymId,
+        userId: request.user.sub,
         userLatitude: latitude,
         userLongitude: longitude,
-        gymId,
-        userId: request.user.sub
     })
-    
+
     return reply.status(201).send()
 }
