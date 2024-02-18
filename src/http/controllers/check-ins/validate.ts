066426d616c0f437f580ac3ef6565966d2dce73b
Nicolas-Moises <nicolas.nicc21@gmail.com>
2024-02-18 01:16:43 -0300

    feat: finish tests e2e

--- src/http/controllers/check-ins/validate.ts
+++ src/http/controllers/check-ins/validate.ts
@@ -4,16 +4,16 @@ import { makeValidateCheckInUseCase } from '@/use-cases/factories/make-validate-
 
 export async function validate(request: FastifyRequest, reply: FastifyReply) {
     const validateCheckInParamsSchema = z.object({
-        checkInId: z.string().uuid()
+        checkInId: z.string().uuid(),
     })
 
     const { checkInId } = validateCheckInParamsSchema.parse(request.params)
 
     const validateCheckInUseCase = makeValidateCheckInUseCase()
-  
+
     await validateCheckInUseCase.execute({
         checkInId,
     })
-    
+
     return reply.status(204).send()
 }
